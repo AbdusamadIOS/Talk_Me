be7ed66641494e48298312d7bb174eff8c022540
abdusamad <abdusamad2609@gmail.com>
2023-11-29 14:22:51 +0500

    Initial Commit

---
+++ Talk_Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talk_Me
+//
+//  Created by Abdusamad Mamasoliyev on 29/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
